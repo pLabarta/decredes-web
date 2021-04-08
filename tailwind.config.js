c6e508306dc5cdc0933e7dab7590a2c706234e3e
plabarta <pablitolabarta@gmail.com>
2021-04-08 18:41:25 -0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        decredblue: '#2970FF',
+        decredgreen: '#2DD8A3',
+        decreddark: '#091440',
+        decredyellow: '#FFC84E'
+      },
+      fontFamily: {
+        'decred': ['Source Sans Pro','sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
